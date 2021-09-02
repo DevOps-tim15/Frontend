afe2dea28063257562b9ac5496ef451db9b675c1
kaca997 <katarina.aleksic97@gmail.com>
2021-09-02 15:29:10 +0200

    Registration frontend added

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { AdminLayoutRoutes } from "./admin-layout.routing";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { RegistrationComponent } from "src/app/pages/auth/registration/registration.component";
+import { NistagramComponent } from "src/app/pages/nistagram/nistagram.component";
+import { RegistrationConfirmationComponent } from "src/app/pages/auth/registration-confirmation/registration-confirmation.component";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    RegistrationComponent,
+    NistagramComponent,
+    RegistrationConfirmationComponent
+  ]
+})
+export class AdminLayoutModule {}
